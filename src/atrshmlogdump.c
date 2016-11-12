008126d9c6b10b91553c249dea44d0975cff5c2c
Anton Rommerskirchen Hydra <ar_hydra@atrsoft.de>
2016-11-12 19:47:29 +0100

    We use now also the COUNT environment if possible
    As already done by the init we can also use the COUNT environment now.
    So we can be called without any parameter, simply need the ID and the COUNT.

--- src/atrshmlogdump.c
+++ src/atrshmlogdump.c
@@ -58,15 +58,28 @@ int main (int argc, char*argv[])
       exit(1);
     }
 
+  const char *s_anz;
+  
   if (argc != 2)
     {
-      printf("usage: %s countbuffer\n", argv[0]);
-      printf("argument count is wrong.\n");
-      printf("logsystem version is %d.\n", ATRSHMLOGVERSION);
-      exit(1);
+      if (ATRSHMLOG_GET_ENV("_COUNT"))
+	{
+	  s_anz = ATRSHMLOG_GET_ENV("_COUNT");
+	}
+      else
+	{
+	  printf("usage: %s countbuffer\n", argv[0]);
+	  printf("parameter count wrong.\n");
+	  printf("logsystem version is %d.\n", ATRSHMLOGVERSION);
+	  exit(1);
+	}
     }
-
-  lanz = strtol(argv[1], 0, 10);
+  else
+    {
+      s_anz = argv[1];
+    }
+  
+  lanz = strtol(s_anz, 0, 10);
   
   printf("shm log attach and dump %d.\n", (int)lanz);
   printf("logsystem version is %d.\n", ATRSHMLOGVERSION);
