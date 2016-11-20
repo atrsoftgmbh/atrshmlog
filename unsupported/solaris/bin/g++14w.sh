c86c46b4bb19be8a7f69c133d78c1b544f481926
Anton Rommerskirchen Hydra <ar_hydra@atrsoft.de>
2016-11-20 18:36:38 +0100

    We have a new platform in unsuported now, the solaris

---
+++ unsupported/solaris/bin/g++14w.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+#!/usr/local/bin/bash
+#!/usr/bin/ksh
+#!/bin/ksh
+#
+# $Id:$
+#
+# Compile the c++ thing and link it
+#
+# check for your platform.
+# if you dont find it add a new one and sent me a postcard with it
+
+if [ -z $ATRSHMLOG_PLATFORM ]
+then
+    echo "please set the environment via dot.platform.sh"
+    exit 1
+fi
+
+case $ATRSHMLOG_PLATFORM in
+    linux)
+	case $ATRSHMLOG_FLAVOUR in
+	    1) # fedora 
+		# linux x86_64 gnu
+		CPP="g++ -std=c++14 -pthread -Wall -Weffc++ -fdump-tree-original  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		LIBMODULE=-latrshmlog
+		;;
+
+	    2) # centos 7.2 with a somewhat outdated g++
+		CPP="g++ -std=gnu++1y -pthread -Wall -Weffc++ -fdump-tree-original"
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		LIBMODULE=-latrshmlog
+		;;
+
+	    *)
+		# linux x86_64 gnu
+		CPP="g++ -std=c++14 -pthread -Wall -Weffc++ -fdump-tree-original  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		LIBMODULE=-latrshmlog
+		;;
+	esac
+
+	;;
+
+    cygwin)
+	# cygwin x86_64 gnu
+	CPP="g++ -std=c++14 -pthread -Wall -Weffc++ -fdump-tree-original"
+	PICFLAG=
+	OPTMODE=-O3
+	LIBMODULE=-latrshmlog
+	;;
+
+    mingw)
+	# mingw x86_64 gnu via cygwin 
+	CPP="x86_64-w64-mingw32-g++ -std=c++14 -pthread -Wall -Weffc++ -fdump-tree-original"
+	PICFLAG= 
+	OPTMODE=-O3
+	LIBMODULE=-latrshmlog
+	;;
+
+    bsd)
+	case $ATRSHMLOG_FLAVOUR in
+	    3)
+		# 
+		CPP="clang++ -std=c++14  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=
+		OPTMODE=-O3
+		LIBMODULE="-latrshmlog  -lpthread"
+		;;
+
+	    4)
+		CPP="clang++ -std=c++14  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=
+		OPTMODE=-O3
+		LIBMODULE="-latrshmlog -lpthread"
+		;;
+
+	    5)
+		CPP="g++ -std=gnu++14  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		LIBMODULE="-latrshmlog -lpthread"
+		;;
+	    
+	    *)
+		CPP="CC "
+		PICFLAG=
+		OPTMODE=-O3
+		LIBMODULE="-latrshmlog "
+		;;
+
+	esac
+	;;
+		
+
+    solaris)
+	CPP="g++ -std=gnu++14  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+	PICFLAG=-fPIC
+	OPTMODE=-O3
+	LIBMODULE="-latrshmlog -lthread -lpthread"
+	;;
+
+    *)
+
+	echo "no platform found. i gave up."
+	exit 1
+	;;
+esac
+
+
+
+
+
+
+#############################################
+
+INFILE=${1%%.cpp}
+INFILE=${INFILE%%.C}
+INFILE=${INFILE%%.c++}
+INFILE=${INFILE}.C
+
+OUTFILE=${INFILE%%.C}
+
+$CPP $PICFLAG -L. $OPTMODE "${INFILE}" $LIBMODULE -o "$OUTFILE"
+
+# end of file
+
---
+++ unsupported/solaris/bin/g99.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#!/usr/local/bin/bash
+#!/usr/bin/ksh
+#!/bin/ksh
+# $Id:$
+#
+# Compile the thing into an object
+#
+# check for your platform.
+# if you dont find it add a new one and sent me a postcard with it
+
+if [ -z $ATRSHMLOG_PLATFORM ]
+then
+    echo "please set the environment via dot.platform.sh"
+    exit 1
+fi
+
+case $ATRSHMLOG_PLATFORM in
+    linux)
+	case $ATRSHMLOG_FLAVOUR in
+	    1) # fedora
+		# linux x86_64 gnu
+		CC="gcc -std=gnu11 -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		;;
+
+	    2) # centos 7.2 with manual 5.4 gcc
+		CC="x86_64-unknown-linux-gnu-gcc-5.4.0 -std=gnu11 -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		;;
+
+	    *)
+		# linux x86_64 gnu
+		CC="gcc -std=gnu11 -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		;;
+
+	esac
+
+	;;
+
+    cygwin)
+	# cygwin x86_64 gnu
+	CC="gcc -std=gnu11"
+	PICFLAG=
+	OPTMODE=-O3
+	;;
+
+    mingw)
+	# mingw x86_64 gnu via cygwin
+	CC="x86_64-w64-mingw32-gcc -std=gnu11"
+	PICFLAG=
+	OPTMODE=-O3
+	;;
+
+    bsd)
+	case $ATRSHMLOG_FLAVOUR in
+	    3)
+		# 
+		CC="clang -std=c11 -fgnu-keywords  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=
+		OPTMODE=-O3
+		;;
+
+	   4)
+		# 
+		CC="clang -std=c11 -fgnu-keywords  -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=
+		OPTMODE=-O3
+		;;
+
+	   5)
+		# 
+		CC="gcc -std=gnu11 -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+		PICFLAG=-fPIC
+		OPTMODE=-O3
+		;;
+
+	    *)
+		# 
+		CC="cc "
+		PICFLAG=
+		OPTMODE=-O3
+		;;
+
+	esac
+	;;
+
+		
+    solaris)
+	# 
+	CC="gcc -std=gnu11 -DATRSHMLOG_FLAVOUR=$ATRSHMLOG_FLAVOUR "
+	PICFLAG=-fPIC
+	OPTMODE=-O3
+	;;
+
+    *)
+
+	echo "no platform found. i gave up."
+	exit 1
+	;;
+esac
+
+
+###############################################
+
+$CC $PICFLAG -I. $OPTMODE $* -c
+
+# end of file
