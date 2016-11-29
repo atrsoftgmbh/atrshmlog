4a587eef1e04b9b9fb23b3c74ec224182ff247e9
Anton Rommerskirchen Hydra <ar_hydra@atrsoft.de>
2016-11-29 10:59:27 +0100

    we dispatch now if we dispose the buffer even if its size is 0

--- src/impls/atrshmlogimpl_flush.c
+++ src/impls/atrshmlogimpl_flush.c
@@ -36,7 +36,7 @@ void atrshmlog_flush(void)
     {
       atrshmlog_tbuff_t* t = g->atrshmlog_targetbuffer_arr[i];
 
-      if(t && t->size > 0)
+      if(t && (t->size > 0 || t->dispose))
 	{
 	  t->number_dispatched = g->number_dispatched++;  // we have a new highest here
  	  
