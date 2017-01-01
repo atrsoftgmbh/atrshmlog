6c69ace90210643fa84e2424320194dc070130ad
Anton Rommerskirchen Hydra <ar_hydra@atrsoft.de>
2017-01-01 19:21:13 +0100

    first version

---
+++ ruby/bin/getfrommain.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#!/usr/bin/ksh
+# $Id:$
+#
+# we get the files from main and populate to all included jdks
+#
+# this is generic so far
+
+if [ -z $ATRSHMLOG_PLATFORM ]
+then
+    echo "please set the environment via dot.platform.sh"
+    exit 1
+fi
+
+case $ATRSHMLOG_PLATFORM in
+    linux)
+	# linux x86_64 gnu
+	LIB=libatrshmlog.a
+	;;
+
+    cygwin)
+	# cygwin x86_64 gnu
+	LIB=libatrshmlog.a
+	;;
+
+    mingw)
+	# mingw x86_64 gnu via cygwin
+	LIB=libatrshmlog.a
+	;;
+
+    *)
+
+	echo "no platform found. i gave up."
+	exit 1
+	;;
+esac
+
+
+cp ../src/atrshmlog.h src/ext/atrshmlog/atrshmlog.h
+cp ../src/atrshmlog_internal.h src/ext/atrshmlog/atrshmlog_internal.h
+cp ../src/$LIB src/ext/atrshmlog/$LIB
+
+
+# end of file
+
+
+
+
