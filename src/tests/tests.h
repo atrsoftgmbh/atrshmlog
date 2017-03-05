93113bad03596b18192478514892cc0648bbe947
Anton Rommerskirchen Hydra <ar_hydra@atrsoft.de>
2017-03-05 14:45:48 +0100

    the test header

---
+++ src/tests/tests.h
@@ -0,0 +1,23 @@
+
+#include "../atrshmlog.h"
+
+#include "../atrshmlog_internal.h"
+
+/*
+ * ------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#define PN(_x) printf( #_x " : %ld :\n", (long) (_x))
+
+#define PS(_x) printf( #_x " : %s :\n", (_x));
+
+#define PP(_x) printf( #_x " : %p :\n", (void*)(_x));
+
+
+
